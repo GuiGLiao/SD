d853d070b6f8b287cf55740a9f6a7a06456c3888
GUILHERME GIMENES LIAO <125086487+GuiGLiao@users.noreply.github.com>
2025-08-12 19:29:10 -0300

    Add files via upload

--- projeto.bat
+++ projeto.bat
@@ -1,6 +1,7 @@
 @echo off
 set ZK="C:\temp\apache-zookeeper-3.9.3-bin"
 set CP_ZK=.;%ZK%\lib\zookeeper-3.9.3.jar;%ZK%\lib\zookeeper-jute-3.9.3.jar;%ZK%\lib\slf4j-api-1.7.30.jar;%ZK%\lib\logback-core-1.2.13.jar;%ZK%\lib\logback-classic-1.2.13.jar;%ZK%\lib\netty-handler-4.1.113.Final.jar
+set LOG="D:\Facul\2025.2\projetoF_SD"
 
 REM Compila todos os arquivos Java
 javac -cp %CP_ZK% codigos\*.java
@@ -11,10 +12,10 @@ set BARRIER_SIZE=3
 set MIN_WORDS=10
 
 
-@echo ***** Formador de Textos *****
+@echo *****Formador de Textos Distribuidos*****
 @echo Tamanho da barreira = %BARRIER_SIZE%
 @echo Quantidade de palavras no texto = %MIN_WORDS%
 @echo.
 
 
-java -Dlogback.configurationFile=file:%ZK%\conf\logback.xml -cp %CP_ZK% codigos.Main %ZK_ADDR% %BARRIER_SIZE% %MIN_WORDS%
+java -Dlogback.configurationFile=file:%LOG%\logback.xml -cp %CP_ZK% codigos.Main %ZK_ADDR% %BARRIER_SIZE% %MIN_WORDS%
